4dd311560314157badfc15a03a799655e2d84e16
CoderWanFeng <875143930@qq.com>
2022-10-17 01:12:46 +0800

    delete wrong course

--- course/PandasVersusExcel-Code/002/2.3-��ȡExcel�ļ����п��У�.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# ⭐Python 60套学习资源：http://t.cn/A6xASARf
-# 📱公众号 :程序员晚枫 读者交流群：http://www.python4office.cn/wechat-group/
-# 🏠2022最新视频：1行代码，实现自动化办公：https://www.bilibili.com/video/BV1pT4y1k7FH
-import pandas as pd
-
-people = pd.read_excel("./example-empty_row.xlsx", header=1)
-# pandas版本1.3.5，无法跳过空行。
-print(people.columns)
