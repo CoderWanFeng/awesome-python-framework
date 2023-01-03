1774bb6fe93b3534a97fe0829bbf0de430b74759
CoderWanFeng <875143930@qq.com>
2023-01-03 14:15:05 +0800

    add 基本数据类型和运算：https://www.bilibili.com/video/BV18g411h7jJ/?p=5

---
+++ 3、给非程序员的Python入门课，0基础适用/code/5、基本数据操作.py
@@ -0,0 +1,36 @@
+# -*- coding: UTF-8 -*-
+'''
+@Author  ：程序员晚枫，B站/抖音/微博/小红书/公众号
+@WeChat     ：CoderWanFeng
+@Blog      ：www.python-office.com
+@Date    ：2023/1/3 13:28 
+@Description     ：
+'''
+
+# def = 1+1
+# class = 1+1
+# a = 1+1
+# print(a)
+
+num = 122
+
+1.666
+
+a = 'abc'
+
+'你好'
+
+ccc = [1, 2, 3]
+
+bbb = {
+    '姓名': "小明",
+    '年龄': "18",
+}
+
+(1, 2)
+
+a = 200
+b = 100
+c = a == b
+print(c)
+
---
+++ 3、给非程序员的Python入门课，0基础适用/code/6、计算器.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+'''
+@Author  ：程序员晚枫，B站/抖音/微博/小红书/公众号
+@WeChat     ：CoderWanFeng
+@Blog      ：www.python-office.com
+@Date    ：2023/1/3 13:50 
+@Description     ：
+'''
+
+a = input("请输入一个整数：")
+
+b = input("请再输入一个整数：")
+
+c = int(a) + int(b)
+
+print(f'两个数相加，结果是：{c}')
+
+
+import pandas as pd
+
+pd.read_excel()
+
+
